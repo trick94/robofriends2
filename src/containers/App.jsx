6323dc7734b16c958d477c139c2136a805666052
John Patrick Pepito <jpepito@crmonline.com.au>
2021-03-30 07:50:41 +0800

    Remove test count button

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,14 +8,12 @@ import ErrorBoundry from '../components/ErrorBoundry';
 function App() {
   const [ robots, setRobots ] = useState([]);
   const [ searchField, setSearchField ] = useState('');
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => setRobots(users));
-        console.log(count);
-  }, [count])
+  }, [])
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value);
@@ -31,7 +29,6 @@ function App() {
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <button onClick={()=>setCount(count + 1)}>Click</button>
         <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
