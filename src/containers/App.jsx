6c4349ce1ef9bb35d46f5841029df561a3d77738
John Patrick Pepito <jpepito@crmonline.com.au>
2021-04-27 09:27:00 +0800

    Code splitting

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,6 @@
-import React, { useEffect } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import './App.css';
-import ErrorBoundry from '../components/ErrorBoundry';
 import { requestRobots, setSearchField } from '../actions';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
+import MainPage from '../components/MainPage';
 
 const mapStateToProps = state => {
   return {
@@ -24,30 +18,8 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-function App({ searchField, robots, isPending, onSearchChange, onRequestRobots }) {
-  useEffect(() => {
-      onRequestRobots();
-  }, [onRequestRobots])
-
-  const filteredRobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase());
-  });
-
-  if (isPending) {
-    return <h1>Loading</h1>
-  } else {
-    return (
-      <div className="tc">
-        <Header/>
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          <ErrorBoundry>
-            <CardList robots={filteredRobots}/>
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    )
-  }
+function App(props) {
+  return <MainPage {...props}/>
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
